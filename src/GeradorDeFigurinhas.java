eb539b0d09239e78c6a7bd910c623fb9e9cdf920
anacirqueira <cirqueiraana6@gmail.com>
2023-04-01 20:26:56 -0300

    Gerando frase na imagem

--- src/GeradorDeFigurinhas.java
+++ src/GeradorDeFigurinhas.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,7 +11,7 @@ public class GeradorDeFigurinhas {
   public void cria() throws Exception {
 
     // leitura da imagem
-    BufferedImage imagemOriginal = ImageIO.read(new File("./entrada/filme.jpg"));
+    BufferedImage imagemOriginal = ImageIO.read(new File("./entrada/filmes.jpg"));
 
     // criar nova imagem em memória com transparência e novo tamanho
     int largura = imagemOriginal.getWidth();
@@ -19,13 +21,24 @@ public class GeradorDeFigurinhas {
 
     // copiar a imagem original pra nova imagem (em memória)
     Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
-    graphics.drawImage(imagemOriginal, null, altura, novaAltura);
+    graphics.drawImage(imagemOriginal, 0, 0, null);
+
+    // configurar a fonte
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 70);
+    graphics.setColor(Color.YELLOW);
+    graphics.setFont(fonte);
 
     // escrever uma frase na imagem
+    graphics.drawString("LEGALZINHO!", 100, novaAltura - 100);
 
     // escrever a nova imagem em um arquivo
-    
+    ImageIO.write(novaImagem, "png", new File("./saida/figurinha.png"));
 
+  }
+
+  public static void main(String[] args) throws Exception {
+    var gerador = new GeradorDeFigurinhas();
+    gerador.cria();
   }
 
 }
